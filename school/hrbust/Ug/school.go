ff771b3ea68fa9294f7aeb54012f3242160a45f1
huhu415 <carzy.dream@outlook.com>
2025-10-19 16:37:33 +0800

    add hrbust nomarl req

---
+++ school/hrbust/Ug/school.go
@@ -0,0 +1,143 @@
+package hrbustUg
+
+import (
+	"context"
+	"errors"
+	"net/http/cookiejar"
+	"strconv"
+	"sync"
+	"time"
+
+	"eduData/repository"
+	school "eduData/school"
+	"eduData/school/pub"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+func (h *HrbustUg) getYearTerm() (year, term string) {
+	now := time.Now()
+	nowYear, nowMonth := now.Year(), now.Month()
+	if nowMonth >= 2 && nowMonth <= 7 {
+		term = "1"
+	} else {
+		term = "2"
+	}
+	year = strconv.Itoa(nowYear - 1980)
+	logrus.Debugf("year: %s, term: %s", year, term)
+	return
+}
+
+type HrbustUg struct {
+	stuID  string
+	passWd string
+	cookie *cookiejar.Jar
+}
+
+func NewHrbustUg(stuID, passWd string, c ...*cookiejar.Jar) school.School {
+	h := HrbustUg{
+		stuID:  stuID,
+		passWd: passWd,
+	}
+	if len(c) == 1 {
+		h.cookie = c[0]
+	}
+	return &h
+}
+
+func (h *HrbustUg) SetCookie(c *cookiejar.Jar) {
+	h.cookie = c
+}
+
+func (h *HrbustUg) SchoolName() pub.SchoolName {
+	return pub.HRBUST
+}
+
+func (h *HrbustUg) StuType() pub.StuType {
+	return pub.UG
+}
+
+func (h *HrbustUg) StuID() string {
+	return h.stuID
+}
+
+func (h *HrbustUg) PassWd() string {
+	return h.passWd
+}
+
+func (h *HrbustUg) Cookie() *cookiejar.Jar {
+	return h.cookie
+}
+
+func (h *HrbustUg) Signin() error {
+	c, err := Signin(h.stuID, h.passWd)
+	if err != nil {
+		return err
+	}
+	h.cookie = c
+	return nil
+}
+
+func (h *HrbustUg) GetCourse() ([]repository.Course, error) {
+	y, t := h.getYearTerm()
+	b, err := GetCourseByTime(h.cookie, y, t)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseDataCrouseAll(b)
+}
+
+func (h *HrbustUg) GetGrade() ([]repository.CourseGrades, error) {
+	if h.cookie == nil {
+		return nil, errors.New("not found the cookie")
+	}
+	// 3个协程获取成绩
+	var grade []repository.CourseGrades
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	errs, ctx := errgroup.WithContext(ctx)
+	msg := make(chan yearSemester, 10)
+	var mutex sync.Mutex
+	for i := 0; i < 3; i++ {
+		errs.Go(func() error {
+			for data := range msg {
+				// 获取页面
+				ugHTML, errUg := GetDataScore(h.cookie, data.Year, data.Semester)
+				if errUg != nil {
+					return errUg
+				}
+				// 从43变成2023这种形式
+				y, _ := strconv.Atoi(data.Year)
+				y = y - 20 + 2000
+
+				// 解析页面, 获得成绩
+				table, errUg := ParseDataSore(ugHTML, strconv.Itoa(y), data.Semester)
+				if errUg != nil {
+					return errUg
+				}
+
+				mutex.Lock()
+				grade = append(grade, table...)
+				mutex.Unlock()
+			}
+			return nil
+		})
+	}
+	// 添加任务, 根据学生学号判断需要获取什么年份的成绩
+	atoiYear, err := strconv.Atoi("20" + h.stuID[0:2])
+	if err != nil {
+		return nil, err
+	}
+	for i := atoiYear; i <= time.Now().Year(); i++ {
+		if i != atoiYear {
+			// 第一年没有春季成绩, 所以不是第一年的时候才添加春季
+			msg <- yearSemester{Year: strconv.Itoa(i%100 + 20), Semester: "1"}
+		}
+		msg <- yearSemester{Year: strconv.Itoa(i%100 + 20), Semester: "2"}
+	}
+
+	close(msg)
+	return grade, errs.Wait()
+}
